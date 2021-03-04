19ca60d267d360103dd09caf9610a6e859892af6
skaraikazuya <2804027138@qq.com>
2021-03-04 22:00:09 +0800

    第二次提交

--- src/main/java/com/mockserver/demo/MockServer.java
+++ src/main/java/com/mockserver/demo/MockServer.java
@@ -5,22 +5,26 @@ import com.mockserver.demo.util.MockUtil;
 import org.mockserver.integration.ClientAndServer;
 import org.mockserver.model.HttpRequest;
 import org.mockserver.model.HttpResponse;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import static org.mockserver.model.Parameter.param;
-
+@SpringBootApplication
 public class MockServer {
     public static void main(String[] args) {
 
-        ClientAndServer server = new ClientAndServer(1080);
-        HttpRequest request = new HttpRequest();
-        request.withPath("/test");
-        request.withPathParameter(param("p","2"));
+        SpringApplication.run(MockServer.class, args);
 
-
-        HttpResponse response = new HttpResponse();
-        response.withBody("2021");
-
-        MockUtil.createExpectation(server,request,response);
+        //ClientAndServer server = new ClientAndServer(1080);
+        //HttpRequest request = new HttpRequest();
+        //request.withPath("/test");
+        //request.withPathParameter(param("p","2"));
+        //
+        //
+        //HttpResponse response = new HttpResponse();
+        //response.withBody("2021");
+        //
+        //MockUtil.createExpectation(server,request,response);
 
     }
 }
--- src/main/java/com/mockserver/demo/controller/MockController.java
+++ src/main/java/com/mockserver/demo/controller/MockController.java
@@ -1,14 +1,34 @@
 package com.mockserver.demo.controller;
 
+import com.mockserver.demo.entity.Mock;
 import com.mockserver.demo.service.MockService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.Map;
 
 @Controller
 public class MockController {
 
     @Autowired
     private MockService mockService;
+
+    @RequestMapping(path = "/create",method = RequestMethod.POST)
+    public String register(Model model, Mock mock){
+
+        Map<String,Object> map = mockService.createMock(mock);
+        if(map != null ){
+            model.addAttribute("usernameMsg",map.get("usernameMsg"));
+            model.addAttribute("passwordMsg",map.get("passwordMsg"));
+            model.addAttribute("emailMsg",map.get("emailMsg"));
+
+        }
+        return "/index";
+    }
+
 
 
 
